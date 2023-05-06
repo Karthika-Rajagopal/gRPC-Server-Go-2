86b814ed49592d0527c70a286b991bae311d1c8d
Karthika Rajagopal <78952344+Karthika-Rajagopal@users.noreply.github.com>
2023-05-06 12:04:30 +0530

    Update main.go

--- client/main.go
+++ client/main.go
@@ -12,12 +12,12 @@ import (
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:4040", grpc.WithInsecure())
+	conn, err := grpc.Dial("localhost:4040", grpc.WithInsecure())   //creates a gRPC connection to the server running on localhost:4040 using grpc Dial method,WithInsecure option is used to establish an unsecured connection without TLS encryption
 	if err != nil {
 		panic(err)
 	}
 
-	client := proto.NewAddServiceClient(conn)
+	client := proto.NewAddServiceClient(conn)   //initializes the client using the generated proto package that contains the gRPC service interface
 
 	g := gin.Default()
 	g.GET("/add/:a/:b", func(ctx *gin.Context) {
@@ -33,7 +33,7 @@ func main() {
 			return
 		}
 
-		req := &proto.Request{A: int64(a), B: int64(b)}
+		req := &proto.Request{A: int64(a), B: int64(b)}  // only if the parsing is successful, it creates a gRPC request using the proto.Request struct and sends it to the gRPC server using the client.Add method
 		if response, err := client.Add(ctx, req); err == nil {
 			ctx.JSON(http.StatusOK, gin.H{
 				"result": fmt.Sprint(response.Result),
